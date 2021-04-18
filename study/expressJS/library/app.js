0c410b78c065de5ea3889eec2421a6683af53bfd
Gustavo Watanabe <gustavo.watanabe@gmail.com>
2021-04-18 12:25:08 +0200

    Finished Mod3

---
+++ study/expressJS/library/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const APP_PORT = 3000;
+
+const app = express();
+
+// MIDDLEWARES:
+// logging
+// app.use(morgan('combined'));
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, 'public')));
+// static
+app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
+
+app.get('/', (req, res) => {
+  // res.send('Hello from my lib app');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+});
+
+app.listen(APP_PORT, () => {
+  debug(`App up and running listening on ${chalk.green(APP_PORT)}`);
+});
