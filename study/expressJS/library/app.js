11dfbf8bd5fb59f542f4e738c4b73d7b85c678d3
Gustavo Watanabe <gustavo.watanabe@gmail.com>
2021-04-18 16:56:15 +0200

    Finished Mod6

--- study/expressJS/library/app.js
+++ study/expressJS/library/app.js
@@ -1,16 +1,27 @@
+// Node Imports/Requires
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
-const appPort = process.env.PORT || 3000;
-
+// App
 const app = express();
+const appPort = process.env.PORT || 3000;
+const appNav = [
+  { title: 'Books', link: '/books' },
+  { title: 'Authors', link: '/authors' },
+];
+
+// Routers
+const bookRouter = require('./src/routes/bookRouter')(appNav);
 
 // MIDDLEWARES:
 // logging
 // app.use(morgan('combined'));
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
 // static
@@ -30,14 +41,15 @@ app.use(
   '/js',
   express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')),
 );
-
-app.set('views', path.join(__dirname, 'src', 'views'));
-app.set('view engine', 'ejs');
+app.use('/books', bookRouter);
 
 app.get('/', (req, res) => {
   // res.send('Hello from my lib app');
   // res.sendFile(path.join(__dirname, 'views', 'index.html'));
-  res.render('index', { list: ['a', 'b'], title: 'MyEJSLib' });
+  res.render('index', {
+    title: 'MyEJSLib',
+    appNav,
+  });
 });
 
 app.listen(appPort, () => {
