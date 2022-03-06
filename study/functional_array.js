964f2670d6336dccc68b85be60b6e794536f4a91
Gustavo Watanabe <gustavo.watanabe@gmail.com>
2022-03-06 17:48:37 +0100

    array funcs

--- study/functional_array.js
+++ study/functional_array.js
@@ -1,3 +1,5 @@
+const newObj = { key1: 8, key2: 64, key3: 512 }
+
 const exampleArray = [
   { key1: 1, key2: 1, key3: 1 },
   { key1: 2, key2: 4, key3: 8 },
@@ -8,7 +10,65 @@ const exampleArray = [
   { key1: 7, key2: 49, key3: 343 },
 ]
 
+// psuh -> append to right, returns length
+console.log(exampleArray.push(newObj))
+
+console.log(exampleArray)
+
+// pop -> remove right, returns last element
+console.log(exampleArray.pop())
+
+console.log(exampleArray)
+
+// shift -> remove left, returns first element
+console.log(exampleArray.shift())
+
+console.log(exampleArray)
+
+// unshift -> append to left, return length
+console.log(exampleArray.unshift({ key1: 1, key2: 1, key3: 1 }))
+
+console.log(exampleArray)
+
+// concat -> extend array, return array extended
+console.log(
+  exampleArray.concat([
+    { key1: 'A', key2: 'AA', key3: 'AAA' },
+    { key1: 'B', key2: 'BB', key3: 'BBB' },
+  ])
+)
+
+// join -> return joined string
+console.log(exampleArray.join('^hooray^'))
+
+// slice -> return new array slice
+console.log(exampleArray.slice(0, 2))
+
+console.log(exampleArray)
+
+// reverse -> mutate and return reversed
+console.log(exampleArray.reverse())
+
+console.log(exampleArray.at(-1))
+
+// element in [indexOf, includes]
+
+console.log(exampleArray.includes({ newObj }))
+console.log(exampleArray.includes(exampleArray[0]))
+
+console.log(exampleArray.indexOf(exampleArray[6]))
+
+// Iterations
+
 console.log(exampleArray.find((set) => set.key2 === 9))
+
+console.log(exampleArray.findIndex((set) => set.key2 === 25))
+
+// all
+console.log(exampleArray.every((set) => set.key1 > 0))
+
+// any
+console.log(exampleArray.some((set) => set.key1 >= 7))
 
 console.log(exampleArray.filter((set) => set.key3 % 2 === 0))
 
