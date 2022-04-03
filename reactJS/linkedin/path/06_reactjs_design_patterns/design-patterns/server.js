b9a611b829e7a7c67df16394105f1533f5ce0d6d
Gustavo Watanabe <gustavo.watanabe@gmail.com>
2022-04-03 12:43:18 +0200

    Chapter 2 done

---
+++ reactJS/linkedin/path/06_reactjs_design_patterns/design-patterns/server.js
@@ -0,0 +1,45 @@
+import express, { json } from 'express'
+import { people as users, products } from './src/utils/dummyData.js'
+
+const app = express()
+
+app.use(json())
+
+let currentUser = [...users].shift()
+
+app.get('/current-user', (req, res) => {
+  res.json(currentUser)
+})
+
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params
+
+  res.json(users.find((user) => user.id === parseInt(id)))
+})
+
+app.post('/users/:id', (req, res) => {
+  const { id } = req.params
+  const { user: updatedUser } = req.body
+
+  users = users.map((user) => (user.id === id ? updatedUser : user))
+
+  res.json(users.find((user) => user.id === id))
+})
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
+
+  res.json(products.find((product) => product.id === parseInt(id)))
+})
+
+app.get('/products', (req, res) => {
+  res.json(products)
+})
+
+app.listen(8080, () => {
+  console.log('Server is listening on port 8080')
+})
--- reactJS/linkedin/path/06_reactjs_design_patterns/design-patterns/src/App.js
+++ reactJS/linkedin/path/06_reactjs_design_patterns/design-patterns/src/App.js
@@ -1,12 +1,22 @@
-import { LargePersonListItem } from './components/people/LargePersonListItem'
-import { LargeProductListItem } from './components/products/LargeProductListItem'
-import { Modal } from './components/Modal'
-import { NumberedList } from './components/NumberedList'
-import { RegularList } from './components/RegularList'
-import { SmallPersonListItem } from './components/people/SmallPersonListItem'
-import { SmallProductListItem } from './components/products/SmallProductListItem'
-import { SplitScreen } from './components/SplitScreen'
-import { people, products } from './utils/dummyData'
+// Chapter 1
+import { LargePersonListItem } from './components/people/LargePersonListItem.js'
+import { LargeProductListItem } from './components/products/LargeProductListItem.js'
+import { Modal } from './components/Modal.js'
+import { NumberedList } from './components/NumberedList.js'
+import { RegularList } from './components/RegularList.js'
+import { SmallPersonListItem } from './components/people/SmallPersonListItem.js'
+import { SmallProductListItem } from './components/products/SmallProductListItem.js'
+import { SplitScreen } from './components/SplitScreen.js'
+import { people, products } from './utils/dummyData.js'
+// Chapter 2
+import axios from 'axios'
+import { CurrentUserLoader } from './containers/CurrentUserLoader.js'
+import { DataSource } from './containers/DataSource.js'
+import { ProductInfo } from './components/products/ProductInfo.js'
+import { ResourceLoader } from './containers/ResourceLoader.js'
+import { UserInfo } from './components/people/UserInfo.js'
+import { UserLoader } from './containers/UserLoader.js'
+// Chapter 3
 
 const LeftHandComponent = ({ children, name }) => (
   <>
@@ -22,37 +32,97 @@ const RightHandComponent = ({ children, message }) => (
   </>
 )
 
+const _getServerData = (url) => async () => {
+  const response = await axios.get(url)
+  return response.data
+}
+
+const _getLocalStorageData = (key) => () => localStorage.getItem(key)
+
+const Text = ({ message }) => <h1>{message}</h1>
+
 const App = () => (
   <>
-    <Modal>
-      <LargeProductListItem product={products[0]} />
-    </Modal>
-    <SplitScreen leftWeight={1} rightWeight={3}>
-      <LeftHandComponent name="Foo">
-        <RegularList
-          items={people}
-          resourceName="person"
-          itemComponent={SmallPersonListItem}
-        />
-        <RegularList
-          items={products}
-          resourceName="product"
-          itemComponent={SmallProductListItem}
-        />
-      </LeftHandComponent>
-      <RightHandComponent message="Bar">
-        <NumberedList
-          items={people}
-          resourceName="person"
-          itemComponent={LargePersonListItem}
-        />
-        <NumberedList
-          items={products}
-          resourceName="product"
-          itemComponent={LargeProductListItem}
-        />
-      </RightHandComponent>
-    </SplitScreen>
+    {/* Chapter 2 - Container Components */}
+    <>
+      {/* Loads 1 specific user from server */}
+      <>
+        <CurrentUserLoader>
+          <UserInfo />
+        </CurrentUserLoader>
+      </>
+      {/* Loads users from server */}
+      <>
+        <UserLoader userId="234">
+          <UserInfo />
+        </UserLoader>
+        <UserLoader userId="345">
+          <UserInfo />
+        </UserLoader>
+      </>
+      {/* Loads resources specifying their type and url */}
+      <>
+        <ResourceLoader resourceUrl="/users/234" resourceName="user">
+          <UserInfo />
+        </ResourceLoader>
+        <ResourceLoader resourceUrl="/products/1234" resourceName="product">
+          <ProductInfo />
+        </ResourceLoader>
+      </>
+      {/* Loads resources abstracting whem from */}
+      <>
+        <DataSource
+          getDataFunc={_getServerData('/users/123')}
+          resourceName="user"
+        >
+          <UserInfo />
+        </DataSource>
+        <DataSource
+          getDataFunc={_getLocalStorageData('message')}
+          resourceName="message"
+        >
+          <Text />
+        </DataSource>
+      </>
+    </>
+    {/* Chapter 1 - Layout Components */}
+    <>
+      {/* Show Modal */}
+      <>
+        <Modal>
+          <LargeProductListItem product={products[0]} />
+        </Modal>
+      </>
+      {/* Reusable split screen and lists */}
+      <>
+        <SplitScreen leftWeight={1} rightWeight={3}>
+          <LeftHandComponent name="Foo">
+            <RegularList
+              items={people}
+              resourceName="person"
+              itemComponent={SmallPersonListItem}
+            />
+            <RegularList
+              items={products}
+              resourceName="product"
+              itemComponent={SmallProductListItem}
+            />
+          </LeftHandComponent>
+          <RightHandComponent message="Bar">
+            <NumberedList
+              items={people}
+              resourceName="person"
+              itemComponent={LargePersonListItem}
+            />
+            <NumberedList
+              items={products}
+              resourceName="product"
+              itemComponent={LargeProductListItem}
+            />
+          </RightHandComponent>
+        </SplitScreen>
+      </>
+    </>
   </>
 )
 
